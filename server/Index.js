d1dfc1e250329a935132c2c09fcfcdb3d2e5fd86
freddyatwira <freddywaigwa@gmail.com>
2022-02-19 17:59:53 +0300

    added __dirname and path

--- server/Index.js
+++ server/Index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
+const path = require('path')
+
 const app = express();
 
-app.get('/', (req, res) =>{
- res.send('<h1>HELLO WORLD!</h1>')
-})
+app.use(express.static(path.join(__dirname, '../public')))
+
+// app.get('/', (req, res) =>{
+//  res.sendFile(path.join(__dirname, '../public', 'index.html'))
+// })
 const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));

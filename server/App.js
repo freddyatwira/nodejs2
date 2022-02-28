5e6d5a9d0c98ca877d8a468c69cd3722c6e5ed36
freddyatwira <freddywaigwa@gmail.com>
2022-02-28 17:32:40 +0300

    added node, express and is working

--- server/App.js
+++ server/App.js
@@ -2,15 +2,57 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
-const mysql = require('mysql');
+const cors = require('cors');
 
 const app = express();
 
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'backend'
+})
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 const port = process.env.PORT || 3005;
 
-app.use(bodyParser.urlencoded({ extended: false}))
+app.get('/todos/', (req, res)=> {
 
-app.use(bodyParser.json())
+    const sqlSelect = "SELECT * FROM todos";
 
-app.listen(port, () =>console.log(`Server listening on port ${port}`))
+    db.query(sqlSelect, (err, result) =>{
+        res.send(result)
+    })
 
+})
+
+app.post('/todos/', (req, res)=> {
+
+    const title = req.body.title;
+
+    const description = req.body.description;
+
+    const sqlInsert = "INSERT INTO todos (title, description) VALUES (?,?);"
+
+    db.query(sqlInsert, [title, description], (err, result) =>{
+        console.log(result)
+    })
+});
+
+app.listen(port, ()=>console.log(`Server running on ${port}`));
+
+
+// const pool = mysql.createPool({
+//     connectionLimit: 100,
+//     host           : 'localhost',
+//     user           : 'root',
+//     password       : '',
+//     database       : 'backend'
+// })
